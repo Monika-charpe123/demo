29b6b3906f4626e1b569b2423aac074fede4fc93
Monika-charpe123 <charpe.monika123@gmail.com>
2024-08-17 10:58:57 +0530

    fourth-commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
-import { AppRoutingModule } from './app-routing.module';
+// Components
 import { AppComponent } from './app.component';
+import { ChatComponent } from './chat';
+import { SearchComponent } from './search';
+import { QueueComponent } from './queue';
+// Ensure correct path if needed
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports: [BrowserModule, FormsModule, HttpClientModule],
+  declarations: [AppComponent, ChatComponent, SearchComponent, QueueComponent],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
